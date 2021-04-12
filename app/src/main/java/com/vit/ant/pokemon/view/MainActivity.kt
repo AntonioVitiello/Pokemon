92d443f99b4880b648cfa9dc0ef4de79920c56bd
vitiello.antonio@gmail.com <p@ssw0rdD!>
2021-04-12 15:10:06 +0200

    Modified double BackPressed to exit

--- app/src/main/java/com/vit/ant/pokemon/view/MainActivity.kt
+++ app/src/main/java/com/vit/ant/pokemon/view/MainActivity.kt
@@ -2,7 +2,7 @@ package com.vit.ant.pokemon.view
 
 import android.os.Bundle
 import android.os.Handler
-import android.os.Message
+import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.vit.ant.pokemon.R
@@ -13,45 +13,31 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var mBackPressedHandler: Handler
+    private var backPressedOnce = false
+    private val mHandler = Handler(Looper.getMainLooper())
+
 
     companion object {
-        const val BACK_PRESSED_WHAT = 1
         const val BACK_PRESSED_DELAY_MILLIS = 1000L
-        var canExit = false
-
-        class BackPressedHandler : Handler() {
-            override fun handleMessage(msg: Message) {
-                when (msg.what) {
-                    BACK_PRESSED_WHAT -> {
-                        canExit = false
-                    }
-                    else -> {
-                        //do nothing
-                    }
-                }
-            }
-        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mBackPressedHandler = BackPressedHandler()
     }
 
     override fun onBackPressed() {
         if (supportFragmentManager.fragments[0].childFragmentManager.backStackEntryCount > 0) {
-            canExit = true
-        }
-        if (canExit) {
-            canExit = false
             super.onBackPressed()
         } else {
-            canExit = true
-            Toast.makeText(this, getString(R.string.press_two_times), Toast.LENGTH_SHORT).show()
+            if (backPressedOnce) {
+                finish()
+            } else {
+                backPressedOnce = true
+                Toast.makeText(this, getString(R.string.press_two_times), Toast.LENGTH_SHORT).show()
+                mHandler.postDelayed({ backPressedOnce = false }, BACK_PRESSED_DELAY_MILLIS)
+            }
         }
-        mBackPressedHandler.sendEmptyMessageDelayed(BACK_PRESSED_WHAT, BACK_PRESSED_DELAY_MILLIS)
     }
 
 }
